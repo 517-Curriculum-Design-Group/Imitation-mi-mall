68a07f40f59824a048ba22d375eb18b0488777ec
Neymarsss <103807537+Neymarsss@users.noreply.github.com>
2023-02-26 00:51:31 +0800

    清空后端代码

--- Server/Xiaomi_mall/src/main/java/com/xiaomi_mall/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.xiaomi_mall.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.xiaomi_mall.enity.User;
-
-public interface UserService extends IService<User> {
-}
