23262b570dc39f93a092daac860c690a3df68bb6
Neymarsss <103807537+Neymarsss@users.noreply.github.com>
2023-02-26 01:36:00 +0800

    后端代码更新

---
+++ Server/xiaomi_mall/src/main/java/com/xiaomi_mall/enity/User.java
@@ -0,0 +1,64 @@
+package com.xiaomi_mall.enity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel("用户实体")
+public class User implements Serializable {
+    private static final long serialVersionUID = -40356785423868312L;
+    
+    @ApiModelProperty("主键")
+    private Long id;
+
+    @ApiModelProperty("用户名")
+    private String userName;
+
+    @ApiModelProperty("昵称")
+    private String nickName;
+
+    @ApiModelProperty("密码")
+    private String password;
+
+    @ApiModelProperty("账号状态（0正常 1停用）")
+    private String status;
+
+    @ApiModelProperty("邮箱")
+    private String email;
+
+    @ApiModelProperty("手机号")
+    private String phonenumber;
+
+    @ApiModelProperty("用户性别（0男，1女，2未知）")
+    private String sex;
+
+    @ApiModelProperty("头像")
+    private String avatar;
+
+    @ApiModelProperty("用户类型")
+    private String userType;
+
+    @ApiModelProperty("创建人的用户id")
+    private Long createBy;
+
+    @ApiModelProperty("创建时间")
+    private Date createTime;
+
+    @ApiModelProperty("更新人")
+    private Long updateBy;
+
+    @ApiModelProperty("更新时间")
+    private Date updateTime;
+
+    @ApiModelProperty("删除标志（0代表未删除，1代表已删除）")
+    private Integer delFlag;
+}
---
+++ Server/xiaomi_mall/src/test/java/com/xiaomi_mall/XiaomiMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaomi_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaomiMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
