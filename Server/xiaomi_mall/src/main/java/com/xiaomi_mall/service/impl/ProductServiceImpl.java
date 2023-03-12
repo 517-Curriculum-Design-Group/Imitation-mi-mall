545fdf118ccd3f8f18b31ae540a310bf7926323f
AshScops <1349143251@qq.com>
2023-03-12 21:41:44 +0800

    Json转map，新增商品vo类

--- Server/xiaomi_mall/src/main/java/com/xiaomi_mall/service/impl/ProductServiceImpl.java
+++ Server/xiaomi_mall/src/main/java/com/xiaomi_mall/service/impl/ProductServiceImpl.java
@@ -1,13 +1,19 @@
 package com.xiaomi_mall.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.Feature;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xiaomi_mall.config.Result;
 import com.xiaomi_mall.exception.enity.Product;
 import com.xiaomi_mall.mapper.ProductMapper;
 import com.xiaomi_mall.service.ProductService;
+import com.xiaomi_mall.util.BeanCopyUtils;
+import com.xiaomi_mall.vo.ProductVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.LinkedHashMap;
 
 @Service
 public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> implements ProductService {
@@ -19,6 +25,15 @@ public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> impl
         LambdaQueryWrapper<Product> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(Product::getProductId, product_id);
         Product product = getOne(queryWrapper);
-        return Result.okResult(product);
+
+        ProductVo pv = new ProductVo();
+        pv.setProductId(product.getProductId());
+        pv.setCategoryId(product.getCategoryId());
+        pv.setProductName(product.getProductName());
+        pv.setSkuList(JSON.parseObject(product.getSkuList(), LinkedHashMap.class, Feature.OrderedField));
+        pv.setProductDescription(product.getProductDescription());
+        pv.setCreateTime(product.getCreateTime());
+
+        return Result.okResult(pv);
     }
 }
---
+++ Server/xiaomi_mall/src/main/java/com/xiaomi_mall/vo/ProductVo.java
@@ -0,0 +1,20 @@
+package com.xiaomi_mall.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductVo {
+    private int productId;
+    private int categoryId;
+    private String productName;
+    private Map<String, Object> skuList = new LinkedHashMap<>();
+    private String productDescription;
+    private LocalDateTime createTime;
+}
