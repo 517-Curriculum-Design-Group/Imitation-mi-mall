0726c998619b08d46bbc53528134b546e6caaecb
Lsscr <57869443+Lsscr@users.noreply.github.com>
2023-03-13 23:42:16 +0800

    前端项目相关修改

--- Client/apps/backstage/src/App.vue
+++ Client/apps/backstage/src/App.vue
@@ -1,30 +1,34 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+console.log(import.meta.env)
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <router-view></router-view>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+body{
+  @apply bg-gray-100;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+#nprogress .bar{
+  background-color: #f4f4f4!important;
+  height: 3px!important;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+::-webkit-scrollbar{
+  width: 4px;
+  height: 6px;
+}
+::-webkit-scrollbar-corner{
+  display: block;
+}
+::-webkit-scrollbar-thumb{
+  border-radius: 8px;
+  background-color: rgba(0, 0, 0, 0.2);
+}
+::-webkit-scrollbar-thumb,
+::-webkit-scrollbar-track{
+  border-right-color: transparent;
+  border-left-color: transparent;
+  background-color: rgba(0, 0, 0, 0.1);
 }
 </style>
