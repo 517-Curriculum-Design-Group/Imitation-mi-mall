1efeb86f5a2e62a248375897264a651fcf999925
“leqaqin <leqaqin@gmail.com>
2023-04-19 21:17:29 +0800

    登陆完善

--- Client/apps/backstage/src/main.js
+++ Client/apps/backstage/src/main.js
@@ -2,10 +2,18 @@ import { createApp } from 'vue'
 import './normalize.css'
 
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import './style.css'
 import 'virtual:windi.css'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(ElementPlus).use(router).mount('#app')
+const app=createApp(App)
+app.use(ElementPlus).use(router)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+
+app.mount('#app')
