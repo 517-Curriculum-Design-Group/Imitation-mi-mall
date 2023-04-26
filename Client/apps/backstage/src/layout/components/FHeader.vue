3446c66e1862a682d6754418b912dbfc3fbec15d
“leqaqin <leqaqin@gmail.com>
2023-04-26 17:17:26 +0800

    顶部和侧边栏开发。

--- Client/apps/backstage/src/layout/components/FHeader.vue
+++ Client/apps/backstage/src/layout/components/FHeader.vue
@@ -1,7 +1,36 @@
 <template>
-    <div class="flex">
+    <div class="f-header">
         <span>
-            
+            小米商城后台
         </span>
+
+    <div class="ml-auto">
+                <el-dropdown>
+            <span class="el-dropdown-link">
+                <el-avatar :size="25" />
+            username
+            <el-icon class="el-icon--right">
+                <arrow-down />
+            </el-icon>
+            </span>
+            <template #dropdown>
+            <el-dropdown-menu>
+                <el-dropdown-item>修改密码</el-dropdown-item>
+                <el-dropdown-item divided>退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+            </template>
+        </el-dropdown>
+    </div>
     </div>
 </template>
+
+<script setup>
+</script>
+
+<style>
+.f-header{
+    @apply flex items-center bg-indigo-500 text-light-50 fixed top-0 left-0 right-0; 
+    height:64px;
+
+}
+</style>
--- Client/apps/backstage/src/layout/components/FMenu.vue
+++ Client/apps/backstage/src/layout/components/FMenu.vue
@@ -1,5 +1,52 @@
 <template>
-    <div>
-        测
-    </div>
+  <div class = "f-menu">
+    <el-menu
+      default-active="2"
+      class="border-0"
+    >
+    <el-menu-item index="1">
+        <el-icon><icon-menu /></el-icon>
+        <span>主控台</span>
+      </el-menu-item>
+
+      <el-sub-menu index="2">
+        <template #title>
+          <el-icon><location /></el-icon>
+          <span>Navigator One</span>
+        </template>
+        <el-menu-item-group title="Group One">
+          <el-menu-item index="1-1">item one</el-menu-item>
+          <el-menu-item index="1-2">item two</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="Group Two">
+          <el-menu-item index="1-3">item three</el-menu-item>
+        </el-menu-item-group>
+        <el-sub-menu index="1-4">
+          <template #title>item four</template>
+          <el-menu-item index="1-4-1">item one</el-menu-item>
+        </el-sub-menu>
+      </el-sub-menu>
+
+
+      <el-menu-item index="3">
+        <el-icon><setting /></el-icon>
+        <span>Navigator Four</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
 </template>
+
+<script setup>
+    
+</script>
+
+<style>
+.f-menu{
+    width:250px;
+    top: 64px;
+    bottom: 0;
+    left: 0;
+    overflow: auto;
+    @apply shadow-md fixed;
+}
+</style>
