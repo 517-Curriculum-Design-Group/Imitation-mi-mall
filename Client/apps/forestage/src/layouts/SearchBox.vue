b2b5deaa3adec229b7e0d68aab477f8b29e2fc7d
Cazacufir <103994575+Cazacufir@users.noreply.github.com>
2023-05-04 23:04:11 +0800

    SearchBox样式更改

--- Client/apps/forestage/src/layouts/SearchBox.vue
+++ Client/apps/forestage/src/layouts/SearchBox.vue
@@ -6,19 +6,22 @@ const value = ref('');
 
 
 <template>
-    <div class="container">
-        <a-input-search v-model:value="value" allowclear placeholder="input search text" @search="onSearch" size="large"
-            class="search w-75" />
+    <div class="flex items-center justify-between w-[1226px] h-[100px] m-auto">
+        <img class="w-[56px] h-[56px]" src="../assets/xiaomilogo.svg" />
+
+        <div class="flex w-[296.6px] h-[49.6px] container">
+            <n-input class="leading-[49.6px] search" v-model:value="value" style="--n-border-hover:1px solid var(--button-background-color)" type="text" placeholder="搜索" />
+            <n-button class="w-[52px] h-[50px] search-btn" style="--n-border-hover:1px solid var(--button-background-color)">
+                <span class="i-mdi-magnify w-[24px] h-[24px]"></span>
+            </n-button>
+        </div>
+
     </div>
 </template>
 
 <style scoped lang="scss">
-.container {
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    width: 79.8%;
-    height: 5.5rem;
-    margin: auto;
+:deep(.search-btn):hover {
+    color: white;
+    background-color: var(--button-background-color);
 }
 </style>
