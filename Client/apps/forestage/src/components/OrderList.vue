7226d07eade15ac7320f3187461d5eb34c0d792e
Cazacufir <103994575+Cazacufir@users.noreply.github.com>
2023-05-05 14:44:56 +0800

    地址选择栏更新（颜色没改

--- Client/apps/forestage/src/components/OrderList.vue
+++ Client/apps/forestage/src/components/OrderList.vue
@@ -1,37 +1,58 @@
 <script setup>
 import { ref } from 'vue'
 const checked = ref(false)
+
+const value = ref(null)
+const options = [
+    {
+        value: "广西",
+        label: "广西",
+        children: [{
+            value: "桂林市",
+            label: "桂林市"
+        },
+        {
+            value: "南宁市",
+            label: "南宁市",
+        }
+        ]
+    }
+]
 </script>
 
 <template>
-    <article class="order-list-box w-79.8% h-5.5rem m-auto">
-        <div class="order-list float-left w-40% h-100%">
-            <li tabindex="1" >综合</li>
-            <li tabindex="1" >新品</li>
-            <li tabindex="1" >销量</li>
-            <li tabindex="1" >价格</li>
+    <article class="order-list-box flex justify-between items-center w-[1226px] h-[30px] m-auto mb-[20px]">
+        <div class="order-list flex items-center w-auto h-[30px]">
+            <li tabindex="1">综合</li>
+            <li tabindex="1">新品</li>
+            <li tabindex="1">销量</li>
+            <li tabindex="1">价格</li>
         </div>
 
-        <form class="type-list float-right h-100% w-50%">
+        <form class="flex items-center flex-row-reverse h-[30px] w-[800px]">
 
-            <input type="checkbox" class="w-1.2rem h-1.2rem border-gray-400 mr-0.4rem" id="btn1">仅看有货
-            <label class="mr-0.4rem ml-2rem" for="btn1"></label>
+            <input type="checkbox" id="btn1">仅看有货
+            <label for="btn1"></label>
 
-            <input type="checkbox" class="w-1.2rem h-1.2rem mr-0.4rem" id="btn2">促销
-            <label class="mr-0.4rem ml-2rem" for="btn2"></label>
+            <input type="checkbox" id="btn2">促销
+            <label for="btn2"></label>
 
-            <input type="checkbox" class="w-1.2rem h-1.2rem mr-0.4rem" id="btn3">分期
-            <label class="mr-0.4rem ml-2rem" for="btn3"></label>
+            <input type="checkbox" id="btn3">分期
+            <label for="btn3"></label>
 
-            <span class="address relative w-7rem h-1.8rem border-solid border-1 border-gray-300 ml-0.4rem"></span>
-            <div class="address-box absolute w-38rem h-18rem
+            <!-- <span class="address relative w-7rem h-1.8rem border-solid border-1 border-gray-300 ml-0.4rem"></span> -->
+            <!-- <div class="address-box  absolute w-38rem h-18rem
              pt-3rem pr-1.5rem pl-1.5rem bg-light-50 top-6.1rem
              border-1 border-solid border-gray-300
-             shadow-lg">
-             <input class="address-input w-full h-2.5rem border-solid border-gray-300 border-1 pl-3rem hover:border-gray-500"
-             placeholder="输入街道、乡镇、小区或商圈名称" >
-            </div>
-            <span>收货地</span>
+             shadow-lg" :class="{show : isShow}"> -->
+            <!-- <input class="address-input w-full h-2.5rem border-solid border-gray-300 border-1 pl-3rem hover:border-gray-500"
+             placeholder="输入街道、乡镇、小区或商圈名称" > -->
+
+            <n-cascader tabindex="1" v-model:value="value" placeholder="请选择地址" expand-trigger='hover' :options="options"
+                check-strategy='child' class="address" style="--n-border-hover:1px solid var(--button-background-color) !important;--n-border-focus:1px solid var(--button-background-color);width:125px;height:30px;"/>
+
+            <!-- </div> -->
+            <span class="mr-[8px]">收货地</span>
 
         </form>
 
@@ -40,11 +61,6 @@ const checked = ref(false)
 </template>
 
 <style scoped lang="scss">
-.order-list {
-    display: flex;
-    align-items: center;
-}
-
 li {
     display: flex;
     justify-content: center;
@@ -64,17 +80,10 @@ li {
         color: var(--button-background-color);
     }
 
-    &:focus{
+    &:focus {
         color: var(--button-background-color);
     }
 
-}
-
-.type-list {
-    display: flex;
-    align-items: center;
-    flex-direction: row-reverse;
-    flex: 1;
 }
 
 input[type="checkbox"] {
@@ -85,6 +94,8 @@ label {
     position: relative;
     height: 1.4rem;
     width: 1.4rem;
+    margin-left: 15px;
+    margin-right: 8px;
     border: 1px solid #e3e3e3;
     cursor: pointer;
 }
@@ -111,20 +122,17 @@ input:checked+label::before {
     color: white;
     opacity: 1;
 }
-.address-box{
-    visibility: hidden;
-}
 
-.address:hover + .address-box,
-.address-box:hover,.address-inpuut:hover{
-    visibility: visible;
-    z-index: 500;
-}
+// .address-box,.show {
+//     visibility: visible;
+// }
 
-.address-input{
-    background: url(https://api.iconify.design/mdi:magnify.svg) no-repeat 100%;
-    background-position: 1rem;
-    z-index:1000;
-}
-
+// .address:hover+.address-box,
+// .address-box:hover,
+// .address-box:has(.address-input:hover)
+// {
+//     visibility: visible;
+//     z-index: 500;
+//     background-color: red;
+// }
 </style>
