9144de3c6a52c0fbdfa5890fcb431a46c6b145f9
AshScops <1349143251@qq.com>
2023-05-05 21:51:15 +0800

    放行商城主页

--- Server/xiaomi_mall/src/main/java/com/xiaomi_mall/config/SecurityConfig.java
+++ Server/xiaomi_mall/src/main/java/com/xiaomi_mall/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 // 对于登录接口 允许匿名访问
                 .antMatchers("/user/login", "/register",
-                "/admin/login", "/v2/api-docs","/getCategoryList", "/getCateProduct",
+                "/admin/login", "/v2/api-docs","/getCategoryList", "/getCateProduct","/getUserHome",
                         "/configuration/ui", "/swagger-resources",
                         "/configuration/security", "/swagger-ui.html",
                         "/webjars/**","/swagger-resources/configuration/ui").anonymous()
