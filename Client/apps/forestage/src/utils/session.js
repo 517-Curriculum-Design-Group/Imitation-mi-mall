189670a5416931f33a02d75b6454a6f4ac980b66
Lsscr <57869443+Lsscr@users.noreply.github.com>
2023-05-06 23:41:28 +0800

    feat: 修改提交

--- Client/apps/forestage/src/utils/session.js
+++ Client/apps/forestage/src/utils/session.js
@@ -6,7 +6,12 @@ export const getSession = (key) => {
   return window.sessionStorage.getItem(key);
 };
 
+export const removeSession = (key) => {
+  return window.sessionStorage.removeItem(key);
+};
+
 export const session = {
   setSession,
   getSession,
+  removeSession,
 };
