7d13832b504859edfe12aa8708fdb6d22fc1b668
“leqaqin <leqaqin@gmail.com>
2023-05-22 11:46:48 +0800

    交接给冉前的最后1t4

--- Client/apps/backstage/src/api/product.js
+++ Client/apps/backstage/src/api/product.js
@@ -8,3 +8,47 @@ export function getproductlist(pageNum, pageSize) {
         }
     })
 }
+
+export function addnewproduct(ProductList){
+    return axios.post('/addNewProduct',ProductList)
+}
+
+
+//商品上下架
+//body:{productId,status}
+
+export function updateproductstatus(Product){
+    return axios.post('/ModifyProductStatus',Product)
+}
+
+//补货
+//body{ List<Integer> skuIds，List<Integer> stocks }
+export function addstock(Product){
+    return axios.post('/addProductStock',Product)
+}
+
+//查看商品sku
+export function getproductsku(productId) {
+    return axios.get(`/getProductSku/${productId}`)
+}
+
+//修改商品SKU接口
+//body{productId,List<ModifySku> skuList,List<ModifySkuDetail> skuDetailList}
+export function updateproductsku(Product){
+    return axios.post('/modifyProductSku',Product)
+}
+
+//删除商品
+//body{数组}
+export function deletproductlist(productId){
+    return axios.delete('/deleteProduct', {
+        data: productId
+    },
+
+        {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+    )
+}
--- Client/apps/backstage/src/main.js
+++ Client/apps/backstage/src/main.js
@@ -12,7 +12,7 @@ import router from './router'
 import './permission'
 
 const app=createApp(App)
-app.use(ElementPlus).use(router).use(store).use(SkuForm)
+app.use(ElementPlus).use(router).use(store)
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
