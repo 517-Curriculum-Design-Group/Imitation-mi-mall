a3b0ab36166d322d498a5cbc58284ec12c1e421d
Neymarsss <103807537+Neymarsss@users.noreply.github.com>
2023-05-23 15:11:25 +0800

    前台秒杀

--- Server/xiaomi_mall/src/main/java/com/xiaomi_mall/controller/PersonController.java
+++ Server/xiaomi_mall/src/main/java/com/xiaomi_mall/controller/PersonController.java
@@ -45,6 +45,7 @@ public class PersonController {
             String avatar = prop.getEndpoint() + "/" + prop.getBucketName() + "/" + objectName;
             User user = userService.addAdvater();
             user.setAvatar(avatar);
+            System.out.println(user);
             return Result.okResult(prop.getEndpoint() + "/" + prop.getBucketName() + "/" + objectName);
         }
         return Result.errorResult(AppHttpCodeEnum.UPLOAD_ERROR);
